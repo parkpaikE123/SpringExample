c87eb0b3fb3158ef8e64ca93aebfe587d40728dd
ryujm <wjdals2285@naver.com>
2025-02-12 20:41:58 +0900

    [류정민] -Mybatis FrameWork를 연동하여 Mysql내에 테이블을 json형태로 출력

--- src/main/java/com/ryujm/spring/ex/database/UsedGoodsController.java
+++ src/main/java/com/ryujm/spring/ex/database/UsedGoodsController.java
@@ -1,17 +1,30 @@
 package com.ryujm.spring.ex.database;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ryujm.spring.ex.database.domain.UsedGoods;
+import com.ryujm.spring.ex.database.service.UsedGoodsService;
 
 // request, response 처리
 @Controller
 public class UsedGoodsController {
 	
+	@Autowired
+	private UsedGoodsService usedGoodsService;
+	
 	// used_goods 테이블의 모든 행을 response에 담는다
+	@ResponseBody
 	@RequestMapping("/db/usedgoods/list")
-	public  usedGoodsList() {
+	public List<UsedGoods> usedGoodsList() {
 		// 중고물품 게시글 정보 얻어오기
+		List<UsedGoods> usedGoodsList = usedGoodsService.getUsedGoodsList();
 		
+		return usedGoodsList;
 	}
 	
 }
--- src/main/java/com/ryujm/spring/ex/database/service/UsedGoodsService.java
+++ src/main/java/com/ryujm/spring/ex/database/service/UsedGoodsService.java
@@ -1,6 +1,12 @@
 package com.ryujm.spring.ex.database.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ryujm.spring.ex.database.domain.UsedGoods;
+import com.ryujm.spring.ex.database.repository.UsedGoodsRepository;
 
 // 로직 (business) 담당
 // 데이터 가공
@@ -9,9 +15,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class UsedGoodsService {
 
+	// 해당 멤버변수에 Spring이 자동으로 객체를 생성해서 <주입>해준다!
+	@Autowired
+	private UsedGoodsRepository usedGoodsRepository;
+	
 	// 중고물품 게시글 모두 얻어 오기
-	public getUsedGoodsList() {
+	public List<UsedGoods> getUsedGoodsList() {
 		// used_goods 테이블 모든 행 조회
+		List<UsedGoods> usedGoodsList = usedGoodsRepository.selectUsedGoodsList();
+		// 데이터 가공
+		return usedGoodsList;
+		
 		
 	}
 	
