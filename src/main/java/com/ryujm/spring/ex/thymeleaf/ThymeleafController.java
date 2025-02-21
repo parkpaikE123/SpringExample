2de80501fe7010838a6c68b7b37fbfe8292df450
ryujm <wjdals2285@naver.com>
2025-02-21 21:27:58 +0900

    [류정민] -thymeleaf, 문자열 메소드 활용, 리스트

--- src/main/java/com/ryujm/spring/ex/thymeleaf/ThymeleafController.java
+++ src/main/java/com/ryujm/spring/ex/thymeleaf/ThymeleafController.java
@@ -1,6 +1,9 @@
 package com.ryujm.spring.ex.thymeleaf;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -48,6 +51,22 @@ public class ThymeleafController {
 		
 		model.addAttribute("userList", userList);
 		return "thymeleaf/ex02";
+	}
+	
+	@GetMapping("/ex03")
+	public String ex03(Model model) {
+		
+		Date date = new Date();
+		
+		LocalDate localDate = LocalDate.now();
+		
+		LocalDateTime localDateTime = LocalDateTime.now();
+		
+		model.addAttribute("date", date);
+		model.addAttribute("localDate", localDate);
+		model.addAttribute("localDateTime", localDateTime);
+		
+		return "thymeleaf/ex03";
 	}
 	
 }
