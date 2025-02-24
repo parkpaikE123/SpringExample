cbc0eeccd6759ff4b17239bf8b9180be63756375
ryujm <wjdals2285@naver.com>
2025-02-24 21:47:17 +0900

    [류정민] -AJAX 실습

---
+++ src/main/java/com/ryujm/spring/ex/ajax/AjaxController.java
@@ -0,0 +1,36 @@
+package com.ryujm.spring.ex.ajax;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AjaxController {
+	
+	// 데이터를 담은 response 구성하는 페이지 (json)
+	// API <-> View (html)
+	// 이름과 생년월일을 전달 받고,
+	// 이름과 나이를 json으로 응답에 담는 페이지
+	@ResponseBody
+	@GetMapping("/ajax/person")
+	public Map<String, Object> personInfo(
+			@RequestParam("name") String name
+			,@RequestParam("birthday") String birthday) {
+		int year = Integer.parseInt(birthday.substring(0, 4));
+		int age = 2025 - year ;
+		// {"name":"김인규","age":29}
+		
+		Map<String, Object> personMap = new HashMap<>();
+		personMap.put("name", name);
+		personMap.put("age",age);
+		
+		return personMap;
+		
+	}
+	
+}
+
