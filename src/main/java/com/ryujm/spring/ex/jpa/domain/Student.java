ec749fc16ce5175d4a7718dd2f40cb9e0dd43dde
ryujm <wjdals2285@naver.com>
2025-02-28 21:45:11 +0900

    [류정민] -JPA 환경설정 및 Getter,Setter 및 builder패턴 예제

---
+++ src/main/java/com/ryujm/spring/ex/jpa/domain/Student.java
@@ -0,0 +1,24 @@
+package com.ryujm.spring.ex.jpa.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@Setter
+public class Student {
+
+	private int id;
+	private String name;
+	private String phoneNumber;
+	private String email;
+	private String dreamJob;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	
+}
